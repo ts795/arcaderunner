25489d09b596f76864849c4452fb5352a1c081d4
Josh Gumperz <joshg3023@gmail.com>
2021-08-12 15:02:30 -0700

    debugged a few issues preventing server.js from running

--- models/index.js
+++ models/index.js
@@ -1,39 +1,36 @@
-const User = require('./User');
-const Friends = require('./Friends');
-const Message = require('./Message')
-const Games = require('./Games')
-const Highscores = require('./Highscores')
-
+const User = require("./User");
+// const Friends = require('./Friends');
+const Message = require("./Message");
+const Games = require("./Games");
+const Highscores = require("./Highscores");
 
 User.hasMany(Message, {
-  foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 User.hasMany(Highscores, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 User.belongsToMany(User, {
-    through: Friends
+  as: "Friends",
+  through: "UserFriends",
 });
 
-Games.hasMany(Highscores, {
-    foreignKey: 'game_id'
-})
+// Games.hasMany(Highscores, {
+//   foreignKey: "game_id",
+// });
 
-Message.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// Message.belongsTo(User, {
+//   foreignKey: "user_id",
+// });
 
-Highscores.belongsToMany(User, {
-    foreignKey: 'user_id'
-});
+// Highscores.belongsToMany(User, {
+//   foreignKey: "user_id",
+// });
 
-Highscores.belongsToMany(Games, {
-    foreignKey: 'game_id'
-});
+// Highscores.belongsToMany(Games, {
+//   foreignKey: "game_id",
+// });
 
-
-
-
-module.exports = { User, Friends, Message, Games, Highscores };
+module.exports = { User, Message, Games, Highscores };
