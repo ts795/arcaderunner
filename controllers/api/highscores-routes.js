480adf5bcaad041bebbf2a531a1fe5351f0299cf
SL <samanthl795@gmail.com>
2021-08-14 16:20:03 -0700

    Add GET route for /api/highscores/:gameId/:maxNumScores

--- controllers/api/highscores-routes.js
+++ controllers/api/highscores-routes.js
@@ -1,8 +1,31 @@
 const router = require("express").Router();
 const path = require("path");
+const { Highscores } = require('../../models');
 
-router.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "../../views/index.html"));
+// Get the high scores for a game in descending order
+// First request parameter is the ID of the game to get high scores for
+// 2nd parameter is the maximum number of high scores to get
+router.get('/:gameId/:maxNumScores', async (req, res) => {
+  try {
+    // Get all of the games
+    const highScoresData = await Highscores.findAll({
+      where: {
+        game_id: parseInt(req.params.gameId)
+      },
+      order: [['score', 'DESC']],
+      limit: parseInt(req.params.maxNumScores)
+    });
+    const highScores = highScoresData.map((highScore) =>
+      highScore.get({ plain: true })
+    );
+    res.json(highScores);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
+
+// Add a high score for a game
+
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,21 @@
 const sequelize = require('../config/connection');
 const gamesData = require('./gameData.json');
-const { Games } = require('../models');
+const userData = require('./userData.json');
+const highscoresData = require('./highscoresData.json');
+const { User, Games, Highscores } = require('../models');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
 
     // Load games for testing
     await Games.bulkCreate(gamesData);
+
+    // Load users for testing
+    await User.bulkCreate(userData);
+
+    // Load Highscores for testing
+    await Highscores.bulkCreate(highscoresData);
+
     process.exit(0);
 };
 
