cd703c83a3563c09e226b3ca821a77f16dfc7247
SL <samanthl795@gmail.com>
2021-08-29 20:00:52 -0700

    Start server on port 3000 so npm run start:dev works

--- react-app/src/components/pages/Profile/index.js
+++ react-app/src/components/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Redirect, useParams } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import jwt_decode from "jwt-decode";
 import { getFavoriteGames } from "../../../utils/API";
 import './Profile.css';
