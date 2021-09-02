6dab1f0fc84425e6ad4c8050648d75e3f5b8ac8b
apal <alishapal96@gmail.com>
2021-09-02 00:22:22 -0700

    add style to game page

--- react-app/src/components/pages/BeforeGames/index.js
+++ react-app/src/components/pages/BeforeGames/index.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Redirect, useParams } from "react-router-dom";
-import { addFavoriteGame, logout, getGame } from "../../../utils/API";
+import { addFavoriteGame, getGame } from "../../../utils/API";
 import './BeforeGames.css';
+
 
 function BeforeGame() {
   const { id } = useParams();
   const [goToGames, setGoToGames] = useState(false);
-  const [goToHome, setGoToHome] = useState(false);
   const [playGame, setPlayGame] = useState(false);
   const [gameInformation, setGameInformation] = useState({});
 
@@ -20,7 +20,6 @@ function BeforeGame() {
   const onBackButtonClick = (e) => {
     setGoToGames(true);
   };
-
   const onFavoriteButtonClick = (e) => {
     console.log("Favorite");
     console.log("id", id);
@@ -28,59 +27,43 @@ function BeforeGame() {
   };
 
   const onPlayButtonClick = (e) => {
+    document.getElementById("monitor").innerHTML = "Hello";
     setPlayGame(true);
   };
 
-  const onLogoutButtonClick = () => {
-    console.log("Logout button");
-    logout().then(() => setGoToHome(true));
-  };
-
-  if (goToGames) {
+   if (playGame) {
+     console.log("hello")
+    // let pathToRedirect = `/gamestart/${id}`;
+    // return <div><Redirect to={pathToRedirect} /></div>;
+  }   else if (goToGames) {
     let pathToRedirect = "/games";
     return <Redirect to={pathToRedirect} />;
-  } else if (goToHome) {
-    let pathToRedirect = "/";
-    return <Redirect to={pathToRedirect} />;
-  } else if (playGame) {
-    let pathToRedirect = `/gamestart/${id}`;
-    return <Redirect to={pathToRedirect} />;
-  } else {
+  } 
+  else {
     console.log("gameInfo", gameInformation);
 
     return (
-      <div class="container">
-        <div class="row">
-          <div class="col">
+   <div className = "page">
+      <div className="container">
+        <div className = "row" id = "game">
+          {/* image */}
+          <div className = "container screenImg">
+          <div id="container">
+          <div id="monitor">
+            <img id = "screen" src={`${process.env.PUBLIC_URL}/arcadescreen.png`} alt = "arcade screen" width = "10px" height = "100px"/>
+          <button onClick={onPlayButtonClick} className = "playBtn">Start Game</button>
+          </div>
+          </div>
+        </div>
+          </div>
+          <div className = "row" id = "gameInfo">
+            <h2>{gameInformation.name ? gameInformation.name : ""}  <button onClick={onFavoriteButtonClick} className = "star">✩</button></h2>
+            <h3>{gameInformation.description ? gameInformation.description : ""} </h3>
             <button onClick={onBackButtonClick}>Back</button>
-            <button onClick={onLogoutButtonClick}>Logout</button>
-            <button onClick={onFavoriteButtonClick}>Favorite</button>
-          </div>
-          <div class="row">
-            <button onClick={onPlayButtonClick}>Play Game</button>
-          </div>
-        </div>
-        <div class="col">
-          <textarea
-            value={gameInformation.name ? gameInformation.name : ""}
-            name="message"
-            type="text"
-            class="message"
-            rows="4"
-            cols="80%"
-          />
-          <textarea
-            value={
-              gameInformation.description ? gameInformation.description : ""
-            }
-            name="message"
-            type="text"
-            class="message"
-            rows="4"
-            cols="80%"
-          />
-        </div>
-      </div>
+
+            </div>
+            </div>
+ </div>
     );
   }
 }
