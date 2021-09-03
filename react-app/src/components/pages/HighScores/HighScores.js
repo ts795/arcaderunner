6dfd5d5c5fda61ba98dafe84d8b6e19bc56c3f5f
SL <samanthl795@gmail.com>
2021-09-02 20:04:15 -0700

    Add navbar to pages that show after logging in

--- react-app/src/components/pages/HighScores/HighScores.js
+++ react-app/src/components/pages/HighScores/HighScores.js
@@ -1,56 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import {Redirect, useParams} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { getAllHighScores } from '../../../utils/API';
 import './highscores.css';
+import Navbar from "../../Navbar";
 
 function HighScores() {
-    const { userId } = useParams();
-      // Get the user's information from locations
-  const [goToProfile, setGoToProfile] = useState(false);
-  const [goToGame, setGoToGame] = useState(false);
   const [highScoresData, setHighScoresData] = useState([]);
 
 
-    useEffect(() => {
-        getAllHighScores().then((result) => {
-        console.log("ALL HIGH SCORES", getAllHighScores);
-        console.log("highscore", result);
-        setHighScoresData(result);
+  useEffect(() => {
+    getAllHighScores().then((result) => {
+      console.log("ALL HIGH SCORES", getAllHighScores);
+      console.log("highscore", result);
+      setHighScoresData(result);
     }
     )
   }, []);
 
 
-    const onProfileButtonClick = (e) => {
-    setGoToProfile(true);
-  };
-  const onGameButtonClick = (e) => {
-    setGoToGame(true);
-  };
+  const listItemsHighScore = highScoresData.map((score) =>
+    <li key={score.id}><div>
+      {score.game.name.toLowerCase() + ": " + score.score}</div><div>{score.user.username.toLowerCase()}</div></li>
+  );
 
-  if (goToProfile) {
-    let pathToRedirect = "/profile/" + userId;
-    return <Redirect to={pathToRedirect} />
-  } else if(goToGame){
-    let pathToRedirect = "/game/" + 4;
-    return <Redirect to={pathToRedirect} />
-  } else {
-        const listItemsHighScore = highScoresData.map((score) =>
-      <li key={score.id}><div>
-        {score.game.name.toLowerCase() + ": " + score.score}</div><div>{score.user.username.toLowerCase()}</div></li>
-    );
-
-    return (
+  return (
+    <div>
+      <Navbar />
       <div id="highScore" className="containerHS">
-        <button onClick={onProfileButtonClick} className="clickableIconHS"><i class="fa fa-user fa-5x" aria-hidden="true"></i></button>
-        <button onClick={onGameButtonClick} className="clickableIconHS"><i class="fa fa-user fa-5x" aria-hidden="true"></i>Game</button>
-         <h3 clasName="logoHS" data-text="high scores">high scores</h3>
+        <h3 clasName="logoHS" data-text="high scores">high scores</h3>
         <ol className="listItemsHS">
           {listItemsHighScore}
         </ol>
       </div>
-    );
-  }
+    </div>
+  );
 
 }
 
--- react-app/src/components/pages/Profile/index.js
+++ react-app/src/components/pages/Profile/index.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Redirect } from 'react-router-dom';
 import jwt_decode from "jwt-decode";
 import { getFavoriteGames } from "../../../utils/API";
-import { removeJSONWebToken } from "../../../utils/auth";
 import './Profile.css';
 import { getHighScores } from '../../../utils/API';
-
+import Navbar from "../../Navbar";
 
 
 function Profile() {
@@ -17,11 +15,8 @@ function Profile() {
   } catch (err) {
     // Token will not exist when logging out
   }
-  const [goToGames, setGoToGames] = useState(false);
-  const [logout, setLogout] = useState(false);
   const [data, setData] = useState([]);
   const [highScoresData, setHighScoresData] = useState([]);
-  const [ goToHighScores, setGoToHighScores] = useState(false);
 
 
   useEffect(() => {
@@ -33,56 +28,36 @@ function Profile() {
   }, []);
 
   useEffect(() => {
-        getHighScores().then((result) => {
-        console.log("highscore", result);
-        setHighScoresData(result);
+    getHighScores().then((result) => {
+      console.log("highscore", result);
+      setHighScoresData(result);
     }
     )
   }, []);
-  const onGamesButtonClick = (e) => {
-    setGoToGames(true);
-  };
 
-  const onLogoutClick = (e) => {
-    removeJSONWebToken();
-    setLogout(true);
-  };
-
-  if (goToGames) {
-    let pathToRedirect = "/games";
-    return <Redirect to={pathToRedirect} />
-  } else if (goToHighScores) {
-    let pathToRedirect = "/highscores/";
-    return <Redirect to={pathToRedirect} />
-  } else if (logout) {
-    let pathToRedirect = "/";
-    return <Redirect to={pathToRedirect} />
-  } else {
-    const listItems = data.map((game) =>
-      <li key={game.id}>{game.name.toLowerCase()}</li>
-    );
-    const listItemsHighScore = highScoresData.map((score) =>
-      <li key={score.id}>{score.game.name.toLowerCase() + ": " + score.score}</li>
-    );
-    return (
-      <div className="profilePageBody">
-        <button onClick={onGamesButtonClick} className="clickableIcon backButton" id="profileBack"><i class="fa fa-arrow-left fa-3x" aria-hidden="true"></i></button>
-        <button onClick={onLogoutClick} className="clickableIcon backButton" id="profileLogout"><i class="fa fa-sign-out fa-3x" aria-hidden="true"></i></button>
-        <div className="profileCard">
-          <h1>{decoded.username.toLowerCase()}</h1>
-          <h3>{decoded.email.toLowerCase()}</h3>
-          <h2>Favorite Games</h2>
-          <ul className="profileFavoriteGames">
-            {listItems}
-          </ul>
+  const listItems = data.map((game) =>
+    <li key={game.id}>{game.name.toLowerCase()}</li>
+  );
+  const listItemsHighScore = highScoresData.map((score) =>
+    <li key={score.id}>{score.game.name.toLowerCase() + ": " + score.score}</li>
+  );
+  return (
+    <div className="profilePageBody">
+      <Navbar />
+      <div className="profileCard">
+        <h1>{decoded.username.toLowerCase()}</h1>
+        <h3>{decoded.email.toLowerCase()}</h3>
+        <h2>Favorite Games</h2>
+        <ul className="profileFavoriteGames">
+          {listItems}
+        </ul>
         <h2>High Scores</h2>
         <ul>
           {listItemsHighScore}
         </ul>
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Profile;
