d3f0829fdf2a822c6b3e9bb80f5a6d4c0f1f1d9e
apal <alishapal96@gmail.com>
2021-09-03 13:38:12 -0700

    add name to gamestart page

--- react-app/src/components/pages/GameStart/index.js
+++ react-app/src/components/pages/GameStart/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import TicTacToe from "../../TicTacToe";
 import RockPaperScissors from "../../RockPaperScissors"
@@ -7,10 +7,19 @@ import InvalidPage from "../../InvalidPage";
 import Hangman from "../../Hangman"
 import War from '../../WarCardGame'
 import "./GameStart.css";
+import { getGame } from "../../../utils/API";
 
 function GameStart() {
   const { gameId } = useParams();
+  const [gameInformation, setGameInformation] = useState({});
   const [goToGames, setGoToGames] = useState(false);
+  useEffect(() => {
+    getGame(gameId).then((result) => {
+      console.log("game", result);
+      setGameInformation(result);
+
+    });
+  }, []);
   const onCancelButtonClick = (e) => {
     setGoToGames(true);
   };
@@ -23,6 +32,7 @@ function GameStart() {
         <button onClick={onCancelButtonClick} id="cancel-btn">
           Cancel
         </button>
+        <h1 className = "GameStartNameRow">Now Playing: {gameInformation.name}</h1>
         {/* <div className="currentGame"> */}
           <div id="gameContainer">
             <div id="gameMonitor">
