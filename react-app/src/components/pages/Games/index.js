108029c78808b0c5ec1f1a256516fc273c627cab
apal <alishapal96@gmail.com>
2021-09-03 19:26:48 -0700

    style games page

--- react-app/src/components/pages/Games/index.js
+++ react-app/src/components/pages/Games/index.js
@@ -31,7 +31,7 @@ function Games() {
         <div className="carouselContainer">
           <Carousel width="100%" centerMode={true} onClickItem={onClickItem}>
             <div gameId="6">
-              <img src={`${process.env.PUBLIC_URL}/CoinFlip.png`} />
+              <img className = "carouselImg" src={`${process.env.PUBLIC_URL}/CoinFlip.png`} />
               <p className="legend">Coin Flip</p>
             </div>
             <div gameId="1">
