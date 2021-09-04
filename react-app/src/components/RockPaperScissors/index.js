dd7f0f218f6c69296635a9108be51b155a3cb796
JessGiannini <jessgiannini@gmail.com>
2021-09-04 15:01:32 -0700

    styling mobile final play again RPS

--- react-app/src/components/RockPaperScissors/index.js
+++ react-app/src/components/RockPaperScissors/index.js
@@ -23,7 +23,7 @@ const RockPaperScissors = () => {
             computerSelection = "scissors";
         }
         let gameResult = winLossTable[selection][computerSelection] ? "won" : (winLossTable[selection][computerSelection] === false ? "lost" : "tied");
-        setMessage(`You chose ${selection} and the computer chose ${computerSelection}.&#13:&#10:You ${gameResult}!`);
+        setMessage(`You chose ${selection} and the computer chose ${computerSelection}. You ${gameResult}!`);
         setGameComplete(true);
     }
     if (!gameComplete) {
