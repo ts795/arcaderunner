89ba37633a233daba3cf5b0bc64b8b14d21f5497
SL <samanthl795@gmail.com>
2021-09-04 21:41:28 -0700

    Make favorite games in profile clickable links

--- react-app/src/components/pages/Profile/index.js
+++ react-app/src/components/pages/Profile/index.js
@@ -4,7 +4,7 @@ import { getFavoriteGames } from "../../../utils/API";
 import './Profile.css';
 import { getHighScores } from '../../../utils/API';
 import Navbar from "../../Navbar";
-
+import { Redirect } from 'react-router-dom';
 
 function Profile() {
   // Get the user's information from locations
@@ -17,7 +17,7 @@ function Profile() {
   }
   const [data, setData] = useState([]);
   const [highScoresData, setHighScoresData] = useState([]);
-
+  const [selectedGameId, setSelectedGameId] = useState("");
 
   useEffect(() => {
     getFavoriteGames().then((result) => {
@@ -35,29 +35,39 @@ function Profile() {
     )
   }, []);
 
+  function handleFavoriteGameClick(gameId) {
+    setSelectedGameId(gameId);
+  }
+
   const listItems = data.map((game) =>
-    <li key={game.id}>{game.name.toLowerCase()}</li>
+    <li className="favoriteGameProfile" key={game.id} onClick={() => handleFavoriteGameClick(game.id)}>{game.name.toLowerCase()}</li>
   );
   const listItemsHighScore = highScoresData.map((score) =>
     <li key={score.id}>{score.game.name.toLowerCase() + ": " + score.score}</li>
   );
-  return (
-    <div className="profilePageBody">
-      <Navbar />
-      <div className="profileCard">
-        <h1>{decoded.username.toLowerCase()}</h1>
-        <h3>{decoded.email.toLowerCase()}</h3>
-        <h2>Favorite Games</h2>
-        <ul className="profileFavoriteGames">
-          {listItems}
-        </ul>
-        <h2>High Scores</h2>
-        <ul>
-          {listItemsHighScore}
-        </ul>
+
+  if (selectedGameId) {
+    let pathToRedirect = "/game/" + selectedGameId;
+    return <Redirect to={pathToRedirect} />
+  } else {
+    return (
+      <div className="profilePageBody">
+        <Navbar />
+        <div className="profileCard">
+          <h1>{decoded.username.toLowerCase()}</h1>
+          <h3>{decoded.email.toLowerCase()}</h3>
+          <h2>Favorite Games</h2>
+          <ul className="profileFavoriteGames">
+            {listItems}
+          </ul>
+          <h2>High Scores</h2>
+          <ul>
+            {listItemsHighScore}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Profile;
